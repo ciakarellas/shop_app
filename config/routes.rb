68831459c782e00f5de08bc7123ced83b8de82db
Ciakarellas <subskrypcje.maciek@gmail.com>
2021-02-09 17:33:54 +0100

    add devise gem and new view for sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "products#index"
   namespace :admin do 
     resources :products
   end
   resources :products, only: [:index, :show]
+  
 end
