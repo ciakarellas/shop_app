6330f7be3ba18220eb4607f90cc5ab426e4fa26d
Ciakarellas <subskrypcje.maciek@gmail.com>
2021-02-13 11:13:40 +0100

    update cart contoller to update method

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,18 @@
 class CartsController < ApplicationController
     def update
-
+        product_in_cart = cart.product_items.find_by(product_id: params[:cart][:product_id])
+        if product_in_cart
+            product_in_cart.update(quantity: params[:cart][:quantity])
+        else
+            ProductItem.create(
+              cart: cart,
+              product: product,
+              name: product.name,
+              price: product.price,
+              quantity: params[:cart][:quantity]
+            )
+        end
+        head :no_content
     end
 
     def show
@@ -12,7 +24,13 @@ class CartsController < ApplicationController
         redirect_to products_path
     end
 
+    private
+
     def cart
         @cart ||= user_signed_in? ? current_user.cart : Cart.find_by(params[:id])
+    end
+
+    def product
+        @product ||= Product.find(params[:cart][:product_id])
     end
 end
